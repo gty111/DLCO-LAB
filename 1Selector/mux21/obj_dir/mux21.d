68fd455435db9d4efc26780065ddcbb827e3f041
gtyinstinct <gtyinstinct@qq.com>
2022-07-22 15:30:22 +0800

    update .gitignore

--- 1Selector/mux21/obj_dir/mux21.d
+++
@@ -1,8 +0,0 @@
-mux21.o: /home/gty/ysyx/DC_lab/1selector/csrc/mux21.cpp \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilatedos.h \
- /usr/local/share/verilator/include/verilated_vcd_c.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilated_trace.h \
- /usr/local/share/verilator/include/verilated_trace_defs.h Vmux21.h \
- /usr/local/share/verilator/include/verilated_heavy.h
